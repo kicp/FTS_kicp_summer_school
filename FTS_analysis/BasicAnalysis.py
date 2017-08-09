e5fee51aea603e5a85075c3c41e51495a6c5b35b
panz <panz@uchicago.edu>
2017-08-09 08:27:56 -0500

    polished

--- FTS_analysis/BasicAnalysis.py
+++ FTS_analysis/BasicAnalysis.py
@@ -10,47 +10,48 @@ import sys
 import numpy as np
 
 
-filename = input('Enter file name: ')
+filename = raw_input('Enter file name: ')
 with open('../Data/' +str(filename) , 'rb') as f:
-    d = pickle.load(f) #Add 'encoding = latin1' if using python 3.x only needed for python2.x
+    d = pickle.load(f) #Add 'encoding = latin1' if using python 3.x  
+def analyze_spectrum(d): 
+	i = 11
+	Nsize = 2**i
+	dt=(1/(d['sample freq'])) #period
+	T1=dt*(Nsize) #full period
+	v=(d['speed'])
+	X = v*T1 #full distance
+	dx = dt*v #smallest amount of distance travelled
+	total_t = (d['scan time']) #how long it ran
     
-i = 11
-Nsize = 2**i
-dt=(1/(d['sample freq'])) #period
-T1=dt*(Nsize) #full period
-v=(d['speed'])
-X = v*T1 #full distance
-dx = dt*v #smallest amount of distance travelled
-total_t = (d['scan time']) #how long it ran
-    
-total_s = (d['samples requested']) #number of samples 
-startpt = ((total_s - Nsize)/2) #starting point
+	total_s = (d['samples requested']) #number of samples 
+	startpt = ((total_s - Nsize)/2) #starting point
 
 
-endpt = startpt + Nsize #ending point
-startpt = int(startpt)
-endpt = int(endpt)
+	endpt = startpt + Nsize #ending point
+	startpt = int(startpt)
+	endpt = int(endpt)
 
-df = 1/T1
-f = df*np.arange(Nsize/2)+df/2.0
-fFull = df*np.arange((Nsize/2) + 1)+df/2.0
-
-
-y = (d['sig0F'])
-D = y[startpt:endpt] #signal
-#D = np.flipud(D)
-
-a = d['delay0F']/v
-t = a[startpt:endpt] #time (used as x axis)
-
-D = np.hanning(Nsize)*D #signal multiplied by a hanning function to improve FFT
-S = np.fft.rfft(D) #fourier transform
-S = S[0:-1]
-u = np.abs(S) #gets rid of imaginary part of fourier transform solely for the plot
-dNu = 1/(Nsize*dx)
-Nu = dNu*np.arange(Nsize/2)
+	df = 1/T1
+	f = df*np.arange(Nsize/2)+df/2.0
+	fFull = df*np.arange((Nsize/2) + 1)+df/2.0
 
 
+	y = (d['sig0F'])
+	D = y[startpt:endpt] #signal
+	#D = np.flipud(D)
+
+	a = d['delay0F']/v
+	t = a[startpt:endpt] #time (used as x axis)
+
+	D = np.hanning(Nsize)*D #signal multiplied by a hanning function to improve FFT
+	S = np.fft.rfft(D) #fourier transform
+	S = S[0:-1]
+	u = np.abs(S) #gets rid of imaginary part of fourier transform solely for the plot
+	dNu = 1/(Nsize*dx)
+	Nu = dNu*np.arange(Nsize/2)
+        return t, D, Nu, u
+
+t, D, Nu, u= analyze_spectrum(d=d)
 f, (ax1,ax2) = pl.subplots(2,1)
 ax1.plot(t,D) #cut off
 
--- FTS_analysis/BasicComparison.py
+++ FTS_analysis/BasicComparison.py
@@ -12,26 +12,66 @@ with open( '../Data/' + str(file1N) , 'rb') as file1:
 file2N = raw_input('Second File Name: ') #gets file 2
 with open( '../Data/' + str(file2N) , 'rb') as file2:
     d2=pickle.load(file2)
-    
-X1=d1['delay0F']
-Y1=d1['sig0F']
 
-X2=d2['delay0F']
-Y2=d2['sig0F']
+
+
+def analyze_spectrum(d): 
+	i = 11
+	Nsize = 2**i
+	dt=(1/(d['sample freq'])) #period
+	T1=dt*(Nsize) #full period
+	v=(d['speed'])
+	X = v*T1 #full distance
+	dx = dt*v #smallest amount of distance travelled
+	total_t = (d['scan time']) #how long it ran
+    
+	total_s = (d['samples requested']) #number of samples 
+	startpt = ((total_s - Nsize)/2) #starting point
+
+
+	endpt = startpt + Nsize #ending point
+	startpt = int(startpt)
+	endpt = int(endpt)
+
+	df = 1/T1
+	f = df*np.arange(Nsize/2)+df/2.0
+	fFull = df*np.arange((Nsize/2) + 1)+df/2.0
+
+
+	y = (d['sig0F'])
+	D = y[startpt:endpt] #signal
+	#D = np.flipud(D)
+
+	a = d['delay0F']/v
+	t = a[startpt:endpt] #time (used as x axis)
+
+	D = np.hanning(Nsize)*D #signal multiplied by a hanning function to improve FFT
+	S = np.fft.rfft(D) #fourier transform
+	S = S[0:-1]
+	u = np.abs(S) #gets rid of imaginary part of fourier transform solely for the plot
+	dNu = 1/(Nsize*dx)
+	Nu = dNu*np.arange(Nsize/2)
+        return t, D, Nu, u
+
+
+t1, D1, Nu1, u1= analyze_spectrum(d1)
+t2, D2, Nu2, u2= analyze_spectrum(d2)
+
+    
+#overplot the spectra without dividing them
 
 pl.figure(1)
-pl.plot(X1,Y1)
+pl.plot(Nu1,u1, label=file1N)
 pl.title(str(file1N)[:-4])
-
-pl.figure(2)
-pl.plot(X2,Y2)
+pl.plot(Nu2,u2, label=file2N)
 pl.title(str(file2N)[:-4])
+pl.legend()
 
 oper = raw_input('Divide [y]/[n] : ')
 
 #ensures the number of samples used is the same to be able to divide corresponding values
-Len1 = len(Y1)
-Len2 = len(Y2)
+Len1 = len(Nu1)
+Len2 = len(Nu2)
 
 Nsize = min(Len1,Len2)
 startpt1 = ((Len1 - Nsize)/2)
@@ -39,12 +79,11 @@ endpt1 = startpt1 + Nsize
 
 startpt2 = ((Len2-Nsize)/2)
 endpt2 = startpt2 + Nsize
-
+# divide the two spectra
 if str(oper) == 'y': 
-    X3 = np.divide(X1[startpt1:endpt1],X2[startpt2:endpt2])
+    u3 = np.divide(u1[startpt1:endpt1],u2[startpt2:endpt2])
     pl.figure(3)
-    pl.plot(X3,Y1)
-    pl.title(str(file1N)[:-4] +'/'+str(file2N)[:-4] )
-
+    pl.plot(Nu1[startpt1:endpt1], u3)
+    pl.title('Divided spectra' )
 
 pl.show()
